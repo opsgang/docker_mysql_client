9b312189deb9294acd8946bc72f4682f0ee2f6ef
jinal--shah <jnshah@gmail.com>
2018-04-02 11:36:47 +0000

    try without -it on docker run

--- t/run.sh
+++ t/run.sh
@@ -14,7 +14,7 @@ default_cmd
 echo "candidate images:"
 docker images | grep candidate
 
-docker run -t --rm --name t1 \
+docker run --rm --name t1 \
     opsgang/aws_mysql_client:candidate /bin/bash -c "mysql -u t -p Pword666 -h localhost -e 'show databases;'"
 
 docker rm -f t1 2>/dev/null || true
