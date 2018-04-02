68d51b95ff4ff708e79a54df99645dd51d6524b8
jinal--shah <jnshah@gmail.com>
2018-04-02 13:52:47 +0000

    force mysql to use tcp instead of local socket

--- t/run.sh
+++ t/run.sh
@@ -13,6 +13,12 @@ docker run -i --rm --name def_out opsgang/aws_mysql_client:candidate
 
 echo "running show databases"
 docker run -i --rm --name t1 \
-    opsgang/aws_mysql_client:candidate mysql -P 3306 -u t -pPword666 -h localhost -e 'show databases;'
+    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP -u t -pPword666 -h localhost -e 'show databases;'
 
 docker rm -f t1 2>/dev/null || true
+
+#docker run -i --rm --name t2 \
+#    -v $PWD/t/fixtures:/fixtures \
+#    -e DB_HOST -e DB_PASS -e DB_USER \
+#    -e FILE=/fixtures/sql.example.gz \
+#    opsgang/aws_mysql_client:candidate /run_sql_from_file.sh
