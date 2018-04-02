474be045d2c77f867620e59cb00d86eee6fd255e
jinal--shah <jnshah@gmail.com>
2018-04-02 11:25:01 +0000

    adding mysql to build

---
+++ t/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# vim: et sr sw=4 ts=4 smartindent:
+#
+# Tests for utility scripts provided with image.
+#
+
+# container should print usage info when no args.
+default_cmd() {
+    export
+}
+
+default_cmd
+
+docker run -it --rm --name t1 \
+    opsgang/aws_mysql_client:candidate mysql -u t -p Pword666 -h localhost -e 'show databases;'
+
+docker rm -f t1 2>/dev/null || true
