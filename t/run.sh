a1a2f723d5c3d7f70bdea335178c88f696e79d21
jinal--shah <jnshah@gmail.com>
2018-04-02 18:48:26 +0000

    added test for dump.sh

--- t/run.sh
+++ t/run.sh
@@ -4,30 +4,35 @@
 # Tests for utility scripts provided with image.
 #
 
-# container should print usage info when no args.
-docker rm -f t1 2>/dev/null || true
-docker rm -f t2 2>/dev/null || true
-docker rm -f def_out 2>/dev/null || true
+RC=0
+for i in 1 2 3 4; do docker rm -f t${i} 2>/dev/null ; done
 
-echo "running default output"
-docker run -i --rm --name def_out opsgang/aws_mysql_client:candidate || exit 1
+echo "=== TEST 1 default entrypoint (help msg)"
+docker run -i --rm --name t1 opsgang/aws_mysql_client:candidate || RC=1
 
-echo "running show databases"
-
+echo "=== TEST 2 bespoke mysql cmd"
 CMD="-u $DB_USER -p$DB_PASS -h $DB_HOST"
-docker run -i --net bridge --rm --name t1 \
-    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP $CMD -e 'show databases;' || exit 1
+docker run -i --net bridge --rm --name t2 \
+    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP $CMD -e 'show databases;' || RC=1
 
-docker rm -f t1 2>/dev/null || true
-
-echo "dir:$PWD/t/fixtures"
-ls -l $PWD/t/fixtures
-echo "running docker fixtures"
-
-docker run -i --rm --name t2 \
+echo "=== TEST 3 /run_sql_from_file.sh"
+docker run -i --rm --name t3 \
     --volumes-from $SHIPPABLE_CONTAINER_NAME \
     -e DB_HOST -e DB_PASS -e DB_USER \
     -e FILE=/fixtures/sql.example.gz \
-    opsgang/aws_mysql_client:candidate /bin/bash -c "ls -l /fixtures ; /run_sql_from_file.sh"
+    opsgang/aws_mysql_client:candidate /run_sql_from_file.sh || RC=1
 
-docker rm -f t2 2>/dev/null || true
+mysql -e "USE example ; show tables;"
+
+echo "=== TEST 4 /dump.sh to GZIPPED FILE"
+export DUMP_OPTS="--opt --add-drop-database --databases example"
+docker run -i --rm --name t3 \
+    --volumes-from $SHIPPABLE_CONTAINER_NAME \
+    -e DB_HOST -e DB_PASS -e DB_USER \
+    -e GZIP=true -e DUMP_OPTS \
+    -e DEST_PATH=/fixtures/example-test.sql.gz \
+    opsgang/aws_mysql_client:candidate /dump.sh || RC=1
+
+for i in 1 2 3 4; do docker rm -f t${i} 2>/dev/null ; done
+
+exit $RC
