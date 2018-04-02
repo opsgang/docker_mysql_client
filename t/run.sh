211d6268992c8c4546319efc02d15ed12e32bbd5
jinal--shah <jnshah@gmail.com>
2018-04-02 21:17:52 +0000

    added final tests

--- t/run.sh
+++ t/run.sh
@@ -3,6 +3,17 @@
 #
 # Tests for utility scripts provided with image.
 #
+# SHARED VOL FOR PERSISTING DATA
+# ==============================
+# ... the /fixtures dir is a shared vol on this shippable
+# container, mapped to /fixtures on other containers.
+#
+# If you want data created during a docker-run to be
+# available for inspection after the container exits
+# store the data under /fixtures.
+# Remember to add --volumes-from $SHIPPABLE_CONTAINER_NAME
+# to your docker run cmd.
+#
 
 setup_db() {
     export DB_HOST=172.17.0.3 DB_PASS=Pword666 DB_USER=tester
@@ -14,24 +25,26 @@ setup_db() {
 
 dc() {
     for i in 1 2 3 4 5; do
-        docker rm -f t${i} 2>/dev/null
+        docker rm -f TEST${i} 2>/dev/null
     done
 }
 
 t_bespoke_cmd() {
-    local t="TEST 2" rc=0
+    local t="TEST2" rc=0
     local cmd="" exp="" out=""
     echo "=== $t bespoke mysql cmd"
     exp="example information_schema mysql performance_schema sys"
     cmd="-u $DB_USER -p$DB_PASS -h $DB_HOST"
     out=$(
         set -o pipefail ;
-        docker run -i --net bridge --rm --name t2 \
+        docker run -i --net bridge --rm --name $t \
         opsgang/$IMG:candidate mysql $cmd -e 'show databases;' \
         | sort || exit 1
     ) || rc=1
 
     diff -wb <(echo $out) <(echo $exp) >/dev/null 2>&1 || rc=1
+
+    docker rm -f $t 2>/dev/null
 
     if [[ $rc -ne 0 ]]; then
         echo "ERROR: $t failed."
@@ -44,22 +57,107 @@ t_bespoke_cmd() {
 }
 
 t_run_gzip_file() {
+    local t="TEST3" rc=0
+    local out=""
 
-    echo "=== TEST 3 /run_sql_from_file.sh (gzipped local)"
-    docker run -i --rm --name t3 \
+    echo "=== $t /run_sql_from_file.sh (gzipped local)"
+    docker run -i --rm --name $t \
         --volumes-from $SHIPPABLE_CONTAINER_NAME \
         -e DB_HOST -e DB_PASS -e DB_USER \
         -e FILE=/fixtures/example.sql.gz \
         opsgang/aws_mysql_client:candidate /run_sql_from_file.sh || rc=1
 
     out=$(mysql -e 'use example; select count(*) AS foo from offices \G ')
-    echo "-->>$out<--"
 
-    rc=1
+    echo "$out" | grep '^foo: 0$' >/dev/null || rc=1
+
+    docker rm -f $t 2>/dev/null
+
     if [[ $rc -ne 0 ]]; then
         echo "ERROR: $t failed."
-        echo "ERROR: got: " $out
-        echo "ERROR: exp: foo --- 0" 
+        echo "ERROR: got: $out"
+        echo "ERROR: exp: foo: 0" 
+        return 1
+    fi
+
+    return 0
+}
+
+t_dump_to_gzip_file() {
+    local t="TEST4" rc=0
+    local out=""
+
+    local out_file=/fixtures/example-test.sql.gz 
+
+    echo "=== $t /dump.sh to GZIPPED FILE"
+    export DUMP_OPTS="--opt --add-drop-database --databases example"
+    docker run -i --rm --name $t \
+        --volumes-from $SHIPPABLE_CONTAINER_NAME \
+        -e DB_HOST -e DB_PASS -e DB_USER \
+        -e GZIP=true -e DUMP_OPTS \
+        -e DEST_PATH=$out_file \
+        opsgang/aws_mysql_client:candidate /dump.sh || rc=1
+
+    zcat $out_file | grep '^CREATE TABLE `products` ($' || rc=1
+
+    docker rm -f $t 2>/dev/null
+
+    if [[ $rc -ne 0 ]]; then
+        echo "ERROR: $t failed."
+        echo "ERROR: file does not create products table? "
+        echo -e "ERROR: file:\nFILE START -->"
+        zcat $out_file
+        echo -e "<-- FILE END"
+        return 1
+    fi
+
+    return 0
+}
+
+# t_dump_to_s3()
+# TEST_STUBS=true will tell code not to actually
+# try to upload to S3.
+t_dump_to_s3() {
+    local t="TEST5" rc=0
+
+    echo "=== $t /dump.sh to S3"
+    export DUMP_OPTS="--opt --add-drop-database --databases example"
+    docker run -i --rm --name $t \
+        --volumes-from $SHIPPABLE_CONTAINER_NAME \
+        -e TEST_STUBS=true \
+        -e DB_HOST -e DB_PASS -e DB_USER \
+        -e GZIP=true -e DUMP_OPTS \
+        -e DEST_PATH=s3://foo/bar/example.sql.gz \
+        opsgang/aws_mysql_client:candidate /dump.sh || rc=1
+
+    docker rm -f $t 2>/dev/null
+
+    if [[ $rc -ne 0 ]]; then
+        echo "ERROR: $t failed. Check output above."
+        return 1
+    fi
+
+    return 0
+}
+
+# t_run_gzip_s3() 
+# TEST_STUBS=true will make container use a t/fixtures file
+# instead of the actual s3 url.
+t_run_gzip_s3() {
+    local t="TEST6" rc=0
+
+    echo "=== $t /run_sql_from_file.sh (gzipped s3)"
+    docker run -i --rm --name $t \
+        --volumes-from $SHIPPABLE_CONTAINER_NAME \
+        -e TEST_STUBS=true \
+        -e DB_HOST -e DB_PASS -e DB_USER \
+        -e FILE=s3://foo/bar/example.sql.gz \
+        opsgang/aws_mysql_client:candidate /run_sql_from_file.sh || rc=1
+
+    docker rm -f $t 2>/dev/null
+
+    if [[ $rc -ne 0 ]]; then
+        echo "ERROR: $t failed. Check output above."
         return 1
     fi
 
@@ -72,10 +170,11 @@ RC=0
 
 dc
 
-# TEST 2
+# TEST 1
 # Container usage msg
 echo "=== TEST 1 default entrypoint (help msg)"
-docker run -i --rm --name t1 opsgang/aws_mysql_client:candidate || RC=1
+docker run -i --rm --name TEST1 opsgang/aws_mysql_client:candidate || RC=1
+docker rm -f TEST1 2>/dev/null
 
 # TEST 2
 # Bespoke cmd: list databases as $OUT, and compare $OUT to $EXP
@@ -87,33 +186,13 @@ t_run_gzip_file || RC=1
 
 # TEST 4
 # Load from file. Check offices table has expected col.
-echo "=== TEST 4 /dump.sh to GZIPPED FILE"
-export DUMP_OPTS="--opt --add-drop-database --databases example"
-docker run -i --rm --name t4 \
-    --volumes-from $SHIPPABLE_CONTAINER_NAME \
-    -e DB_HOST -e DB_PASS -e DB_USER \
-    -e GZIP=true -e DUMP_OPTS \
-    -e DEST_PATH=/fixtures/example-test.sql.gz \
-    opsgang/aws_mysql_client:candidate /dump.sh || RC=1
+t_dump_to_gzip_file || RC=1
 
-echo "=== TEST 5 /dump.sh to S3"
-export DUMP_OPTS="--opt --add-drop-database --databases example"
-docker run -i --rm --name t5 \
-    --volumes-from $SHIPPABLE_CONTAINER_NAME \
-    -e TEST_STUBS=true \
-    -e DB_HOST -e DB_PASS -e DB_USER \
-    -e GZIP=true -e DUMP_OPTS \
-    -e DEST_PATH=s3://foo/bar/example.sql.gz \
-    opsgang/aws_mysql_client:candidate /dump.sh || RC=1
+# TEST 5
+t_dump_to_s3 || RC=1
 
-echo "=== TEST 6 /run_sql_from_file.sh (gzipped s3)"
-docker run -i --rm --name t3 \
-    --volumes-from $SHIPPABLE_CONTAINER_NAME \
-    -e TEST_STUBS=true \
-    -e DB_HOST -e DB_PASS -e DB_USER \
-    -e FILE=s3://foo/bar/example.sql.gz \
-    opsgang/aws_mysql_client:candidate /run_sql_from_file.sh || RC=1
-
+# TEST 6
+t_run_gzip_s3 || RC=1
 dc
 
 exit $RC
