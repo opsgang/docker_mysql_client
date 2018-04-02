f4a4e3418d410b2fb89cc2308c4ba775049b2f94
jinal--shah <jnshah@gmail.com>
2018-04-02 18:10:33 +0000

    try

--- t/run.sh
+++ t/run.sh
@@ -8,27 +8,19 @@
 docker rm -f t1 2>/dev/null || true
 docker rm -f def_out 2>/dev/null || true
 
-#echo "running default output"
-#docker run -i --rm --name def_out opsgang/aws_mysql_client:candidate
+echo "running default output"
+docker run -i --rm --name def_out opsgang/aws_mysql_client:candidate || exit 1
 
 echo "running show databases"
 
-echo "trying 172.17.0.3"
+CMD="-u $DB_USER -p$DB_PASS -h $DB_HOST"
 docker run -i --net bridge --rm --name t1 \
-    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP -u tester -pPword666 -h 172.17.0.3 -e 'show databases;'
-
-echo "trying 172.17.0.1 with wrong user name"
-docker run -i --net bridge --rm --name t1 \
-    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP -u t -pPword666 -h 172.17.0.1 -e 'show databases;'
-
-echo "trying 172.17.0.3 with correct user name"
-docker run -i --net bridge --rm --name t1 \
-    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP -u tester -pPword666 -h 172.17.0.3 -e 'show databases;'
+    opsgang/aws_mysql_client:candidate mysql -P 3306 --protocol=TCP $CMD -e 'show databases;' || exit 1
 
 docker rm -f t1 2>/dev/null || true
 
-#docker run -i --rm --name t2 \
-#    -v $PWD/t/fixtures:/fixtures \
-#    -e DB_HOST -e DB_PASS -e DB_USER \
-#    -e FILE=/fixtures/sql.example.gz \
-#    opsgang/aws_mysql_client:candidate /run_sql_from_file.sh
+docker run -i --rm --name t2 \
+    -v $PWD/t/fixtures:/fixtures \
+    -e DB_HOST -e DB_PASS -e DB_USER \
+    -e FILE=/fixtures/sql.example.gz \
+    opsgang/aws_mysql_client:candidate /run_sql_from_file.sh
